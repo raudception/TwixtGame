0ab11c99e3a3f5e0b0d1e050e79c779b81336f47
SeanGillespie <gillespi20@up.edu>
2017-12-08 15:40:54 -0800

    AI shoulf be in its final form

--- app/src/main/java/edu/up/cs301/twixt/TwixtDumbPlayer.java
+++ app/src/main/java/edu/up/cs301/twixt/TwixtDumbPlayer.java
@@ -10,13 +10,20 @@ import edu.up.cs301.game.infoMsg.GameInfo;
 
 /**
  * Created by Sean on 11/14/2017.
+ *
+ * This class is a dumb computer player that makes random moves
+ * However, it can win if given enough moves (I.E. hundreds)
  */
 
 public class TwixtDumbPlayer extends GameComputerPlayer {
 
-    private ArrayList<Peg> copy;//copy arraylist of linked pegs, empty
+    int xVal;//random x position for peg
+    int yVal;//random y  position for peg
+    private Peg[][] current = new Peg[24][24];//array with current peg placements
+    private TwixtGameState turnState;//the state of the board on this turn
+    private Peg thisTurnPeg;//teh peg being placed this turn
 
-    /*
+    /**
      * Constructor for the TwixtDumbPlayer class
      */
     public TwixtDumbPlayer(String name) {
@@ -34,20 +41,30 @@ public class TwixtDumbPlayer extends GameComputerPlayer {
      */
     public void receiveInfo(GameInfo info) {
 
+        if (info instanceof TwixtGameState) {
+            //converts arraylist into 2d array of the board
+            turnState = (TwixtGameState) info;
+            current = turnState.stateToArray();
+        }
+
         // pick peg positions at random on the board
         Random r = new Random();
-        int xVal = r.nextInt(24);//random x value, math may not be right as of now
-        int yVal = r.nextInt(24);//random y value, math may not be right as of now.
 
-
-
+        if (this.playerNum == 0) {//if AI is player 0
+            xVal = r.nextInt(22) + 1;//random x value that is valid
+            yVal = r.nextInt(24);//random y value that is valid
+        } else if (this.playerNum == 1) {//if AI is player 1
+            xVal = r.nextInt(24);//random x value that is valid
+            yVal = r.nextInt(22) + 1;//random y value that is valid
+        }
 
 
         // delay for a second to simulate thinking
-        sleep(2000);
+        sleep(1000);
 
-        Peg thisTurnPeg = new Peg(xVal, yVal, this.playerNum);//peg object that is being placed on this turn
-
+        if (current[xVal][yVal] == null) {
+            thisTurnPeg = new Peg(xVal, yVal, this.playerNum);//peg object that is being placed on this turn
+        }
         // Submit our move to the game object. We haven't even checked it it's
         // our turn, or that that position is unoccupied.
         game.sendAction(new PlacePegAction(this, thisTurnPeg));//the this may not be correct
